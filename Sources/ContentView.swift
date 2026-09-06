d86c519e843ab41cae74d98643873420ee11b1c2
DEV-ismoil <scratchistyt1@gmail.com>
2026-09-06 16:36:56 -0500

    Fix indentation and formatting in ContentView.swift

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -22,15 +22,16 @@ struct WebView: UIViewRepresentable {
         config.allowsInlineMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
 
-let webView = WKWebView(frame: .zero, configuration: config)
+        let webView = WKWebView(frame: .zero, configuration: config)
 
-if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "WebAssets") {
-    webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
-} else if let url = URL(string: "https://groove.ismail.works") { // Fallback URL if local assets fail
-    webView.load(URLRequest(url: url))
-}
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "WebAssets") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        } else if let url = URL(string: "https://groove.ismail.works") {
+            webView.load(URLRequest(url: url))
+        }
 
-return webView
+        return webView
+    }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {}
 }
